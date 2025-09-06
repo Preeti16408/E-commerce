065025e3066e39868a7829b1c0efdf308e499257
Preeti16408 <guptapreeti3256@gmail.com>
2025-09-06 11:53:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins","sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#E6C744"
+    },
+    screens: {
+      sm: "480px",
+  },
+  content: {
+    oval:'url("banner-image-2.png")',
+  },
+},
+},
+  plugins: [],
+};
